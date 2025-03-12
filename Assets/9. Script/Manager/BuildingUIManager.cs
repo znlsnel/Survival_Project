19ba38d0dea9ea4a8aadc57333bd2501c7334477
Jung Ha Na <73771420+onehana1@users.noreply.github.com>
2025-03-12 22:51:01 +0900

    🔨 Refactor: 빌딩UI와 빌딩UI매니저 분리

--- Assets/9. Script/Manager/BuildingUIManager.cs
+++ Assets/9. Script/Manager/BuildingUIManager.cs
@@ -1,22 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BuildingUIManager : MonoBehaviour
 {
-    [Header("UI References")]
-    [SerializeField] private Button funiButton;
-    [SerializeField] private Button decoButton;
-
-    [SerializeField] private GameObject buildingMenuUI;
-    [SerializeField] private GameObject buildingDecUI;
-    [SerializeField] private Transform listBG;  // 리스트
-    [SerializeField] private GameObject buttonPrefab;   // 건설품 버튼
-
-
     [Header("Building Data")]
-    [SerializeField] private List<BuildingData> allBuildings;   // 모든 건설품들 목록
+    [SerializeField] private List<BuildingData> allBuildings; // 모든 건설품 목록
 
+    private BuildingUI buildingUI;
+
+    private void Awake()
+    {
+        buildingUI = FindObjectOfType<BuildingUI>();
+    }
 
     private void OnEnable()
     {
@@ -28,61 +23,27 @@ public class BuildingUIManager : MonoBehaviour
         EventManager.Instance.OnBuildingModeChanged -= HandleBuildingModeChange;
     }
 
-
     private void Start()
     {
-        funiButton.onClick.AddListener(() => ShowBuildingsByCategory(BuildingType.Furniture));
-        decoButton.onClick.AddListener(() => ShowBuildingsByCategory(BuildingType.Decoration));
+        if (buildingUI == null)
+        {
+            Debug.LogError("BuildingUI가 할당되지 않았습니다.");
+            return;
+        }
 
+        buildingUI.InitializeUI(this);
         ShowBuildingsByCategory(BuildingType.Furniture);
-        buildingMenuUI.SetActive(false);
     }
 
     private void HandleBuildingModeChange(bool isBuildingMode)
     {
-        buildingMenuUI.SetActive(isBuildingMode);
+        if (buildingUI != null)
+            buildingUI.ToggleUI(isBuildingMode);
     }
 
-    private void ShowBuildingsByCategory(BuildingType category)
+    public void ShowBuildingsByCategory(BuildingType category)
     {
-        if (listBG == null || buttonPrefab == null)
-        {
-            Debug.LogError("listBG 또는 buttonPrefab이 설정되지 않았습니다.");
-            return;
-        }
-
-        foreach (Transform child in listBG)
-        {
-            Destroy(child.gameObject);
-        }
-
-        foreach (BuildingData building in allBuildings)
-        {
-            if (building == null)
-            {
-                Debug.LogError("BuildingData가 null입니다.");
-                continue;
-            }
-
-            if (building.buildingType == category)
-            {
-                GameObject newButton = Instantiate(buttonPrefab, listBG);
-                Text buttonText = newButton.GetComponentInChildren<Text>();
-
-                if (buttonText != null)
-                    buttonText.text = building.buildingName;
-
-                Button btn = newButton.GetComponent<Button>();
-                if (btn != null)
-                    btn.onClick.AddListener(() => SelectBuilding(building));
-            }
-        }
+        if (buildingUI != null)
+            buildingUI.UpdateBuildingList(allBuildings, category);
     }
-
-
-    private void SelectBuilding(BuildingData building)
-    {
-        Debug.Log($"선택된 건축물: {building.buildingName}");
-    }
-
 }
---
+++ Assets/9. Script/UI/BuildingUI.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingUI : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private Button funiButton;
+    [SerializeField] private Button decoButton;
+    [SerializeField] private GameObject buildingMenuUI;
+    [SerializeField] private Transform listBG;      // 리스트
+    [SerializeField] private GameObject buttonPrefab;   // 건설품 버튼
+
+    private BuildingUIManager buildingUIManager;
+
+    public void InitializeUI(BuildingUIManager manager)
+    {
+        buildingUIManager = manager;
+
+        funiButton.onClick.AddListener(() => buildingUIManager.ShowBuildingsByCategory(BuildingType.Furniture));
+        decoButton.onClick.AddListener(() => buildingUIManager.ShowBuildingsByCategory(BuildingType.Decoration));
+
+        ToggleUI(false);
+    }
+
+    public void ToggleUI(bool isBuildingMode)
+    {
+        buildingMenuUI.SetActive(isBuildingMode);
+    }
+
+    public void UpdateBuildingList(List<BuildingData> allBuildings, BuildingType category)
+    {
+        foreach (Transform child in listBG)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (BuildingData building in allBuildings)
+        {
+            if (building.buildingType == category)
+            {
+                GameObject newButton = Instantiate(buttonPrefab, listBG);
+                Text buttonText = newButton.GetComponentInChildren<Text>();
+
+                if (buttonText != null)
+                    buttonText.text = building.buildingName;
+
+                Button btn = newButton.GetComponent<Button>();
+                if (btn != null)
+                    btn.onClick.AddListener(() => SelectBuilding(building));
+            }
+        }
+    }
+
+    private void SelectBuilding(BuildingData building)
+    {
+        Debug.Log($"선택된 건축물: {building.buildingName}");
+    }
+}
