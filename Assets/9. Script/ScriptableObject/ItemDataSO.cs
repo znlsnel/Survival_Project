7dd9abff817791e0e6bb511cac154d6cf10631f8
Udi <dnel0605@gmail.com>
2025-03-13 15:10:04 +0900

    ✨ Feat: Weapon Item

--- Assets/9. Script/ScriptableObject/ItemDataSO.cs
+++ Assets/9. Script/ScriptableObject/ItemDataSO.cs
@@ -1,21 +1,38 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public enum EItemType
+{ 
+    Weapon,
+    Consumable,
+    Resource,
+}
+
+
 [CreateAssetMenu(fileName = "new ItemData", menuName = "My ScriptableObject/ItemData")]
 public class ItemDataSO : ScriptableObject
 {
-    [Header ("Item Data")]
-    [SerializeField] private Sprite itemIcon;
+    [Header ("Item Info")]
+	[SerializeField] private EItemType itemType;
+	[SerializeField] private Sprite itemIcon;
     [SerializeField] private string itemName; 
     [SerializeField] private string itemDescription;
     [SerializeField] private GameObject dropItemPrefab;
 
+	[Header("Weapon Info")]
+	[SerializeField] private GameObject weaponPrefab;
+
+	public EItemType ItemType => itemType;
     public Sprite ItemIcon => itemIcon;
     public string ItemName => itemName;
     public string ItemDescription => itemDescription;
     public GameObject DropItemPrefab => dropItemPrefab;
+    public GameObject WeaponPrefab => weaponPrefab;
 
-    
+   
 }
  
