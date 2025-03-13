8984785f4a5db36326ca216d1bec700f4fb9655e
hee103 <gmlgml3213@gmail.com>
2025-03-13 17:24:54 +0900

    ✨ Feat: “체온에 따른 UI 색상 변경"

--- Assets/9. Script/Condition/UICondition.cs
+++ Assets/9. Script/Condition/UICondition.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UICondition : MonoBehaviour
 {
@@ -10,9 +11,25 @@ public class UICondition : MonoBehaviour
     public Conditions stamina;
     public Conditions temperature;
 
+    public Image temperatureBar;  // temperatureBar¸¦ ¿¬°á
+
     private void Start()
     {
         TestCharacterManager.Instance.Player.condition.uiCondition = this;
     }
 
+    private void Update()
+    {
+        if (temperature.curValue <= 50)
+        {
+            float lerpValue = Mathf.InverseLerp(0, 50, temperature.curValue);
+            temperatureBar.color = Color.Lerp( Color.blue, Color.green, lerpValue);
+        }
+        else
+        {
+            float lerpValue = Mathf.InverseLerp(50, 100, temperature.curValue);
+            temperatureBar.color = Color.Lerp(Color.green, Color.red, lerpValue);
+        }
+    }
 }
+
