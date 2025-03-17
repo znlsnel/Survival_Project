5fa9399b04ba59991f12fb75429a83719eead84a
hee103 <gmlgml3213@gmail.com>
2025-03-17 17:46:03 +0900

    ✨ Feat: " condition UI 재연결"

--- Assets/9. Script/Condition/UICondition.cs
+++ Assets/9. Script/Condition/UICondition.cs
@@ -9,28 +9,23 @@ public class UICondition : MonoBehaviour
     public Conditions stamina;
     public Conditions temperature;
 
-    public Slider temperatureBar;  // 슬라이더 오브젝트
-    private Image fillImage;  // Fill 영역의 이미지
+    public Image temperatureBar;
 
-    private void Start()
-    {
-        // 슬라이더에서 Fill 영역의 Image 가져오기
-        fillImage = temperatureBar.fillRect.GetComponent<Image>();
-    }
 
     private void Update()
     {
-        if (fillImage == null) return;
+
 
         if (temperature.curValue <= 50)
         {
             float lerpValue = Mathf.InverseLerp(0, 50, temperature.curValue);
-            fillImage.color = Color.Lerp(Color.blue, Color.green, lerpValue);
+            temperatureBar.color = Color.Lerp(Color.blue, Color.green, lerpValue);
         }
         else
         {
             float lerpValue = Mathf.InverseLerp(50, 100, temperature.curValue);
-            fillImage.color = Color.Lerp(Color.green, Color.red, lerpValue);
+            temperatureBar.color = Color.Lerp(Color.green, Color.red, lerpValue);
         }
     }
 }
+
