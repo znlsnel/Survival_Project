85f8cd20697fb504283f7b7f5482af58f3dbb2bc
Udi <dnel0605@gmail.com>
2025-03-18 21:22:04 +0900

    ✨ Feat: 컷신 적용 (게임 종료)

--- Assets/9. Script/Handler/InventoryHandler.cs
+++ Assets/9. Script/Handler/InventoryHandler.cs
@@ -105,7 +105,10 @@ public class InventoryHandler : MonoBehaviour
 		onChangedSlot?.Invoke();
 		messageUI.AddItem(item); 
 		QuestManager.ProgressQuest(EQuestCategory.Pickup, item.ItemName); 
-
+		if (item.ItemName == "¿£Áø")
+		{
+			GameManager.Instance.GameEnd();
+		}
 		
 		return true;
 	}
