1611249209ac51c5cacc1e459d33bcc45f4f83ca
hee103 <gmlgml3213@gmail.com>
2025-03-18 20:46:02 +0900

    📝 Docs: “작업한 스크립트 주석 추가”

--- Assets/9. Script/Condition/Conditions.cs
+++ Assets/9. Script/Condition/Conditions.cs
@@ -4,34 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Conditions : MonoBehaviour
 {
-    public float curValue;
-    public float maxValue;
-    public float startValue;
-    public float passiveValue;
+    // 상태 변수
+    public float curValue; // 현재 값
+    public float maxValue; // 최대 값
+    public float startValue; // 시작 값
+    public float passiveValue; // 패시브 변화량
     public Slider uiBar;
 
     private void Start()
     {
+        // 현재 값을 초기 값으로 설정
         curValue = startValue;
     }
 
     private void Update()
     {
+        // UI 바 값을 현재 상태 비율로 업데이트
         uiBar.value= GetPercentage();
-       
-
     }
 
     public float GetPercentage()
     {
+        // 현재 값을 최대 값 대비 퍼센트로 변환
         return maxValue > 0 ? curValue / maxValue : 0f;
     }
 
+    // 상태 증가
     public void Add(float amount)
     {
         curValue = Mathf.Min(curValue + amount, maxValue);
     }
 
+    // 상태 감소
     public void Subtract(float amount)
     {
         curValue = Mathf.Max(curValue - amount, 0f);
--- Assets/9. Script/Condition/UIWeather.cs
+++ Assets/9. Script/Condition/UIWeather.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class UIWeather : MonoBehaviour
 {
     private Weather weather;
+
+    // 날씨에 따른 UI 요소
     public GameObject sunny;
     public GameObject rainy;
     public GameObject hot;
     public GameObject snow;
+
+    // 날씨 파티클 
     public ParticleSystem rainParticle;
     public ParticleSystem snowParticle;
+
+    // 카메라 위치 참조
     private Transform cameraTransform;
 
     void Start()
@@ -20,21 +26,22 @@ public class UIWeather : MonoBehaviour
         rainParticle = GameObject.Find("Rain").GetComponent<ParticleSystem>();
         snowParticle = GameObject.Find("Snow").GetComponent<ParticleSystem>();
 
-
+        // 메인 카메라의 Transform 가져오기
         cameraTransform = Camera.main.transform;
     }
 
     void Update()
     {
+        // 모든 날씨 UI 요소 초기화
         sunny.SetActive(false);
         rainy.SetActive(false);
         hot.SetActive(false);
         snow.SetActive(false);
 
-        if (rainParticle != null) rainParticle.Stop();
-        if (snowParticle != null) snowParticle.Stop();
+        rainParticle.Stop();
+        snowParticle.Stop();
 
-
+        // 현재 날씨 상태에 따른 UI 활성화 및 파티클 적용
         switch (weather.currentWeather)
         {
             case Weather.WeatherType.Sunny:
@@ -42,7 +49,7 @@ public class UIWeather : MonoBehaviour
                 break;
             case Weather.WeatherType.Rainy:
                 rainy.SetActive(true);
-                if (rainParticle != null) rainParticle.Play();
+                rainParticle.Play();
 
                 break;
             case Weather.WeatherType.Hot:
@@ -50,18 +57,16 @@ public class UIWeather : MonoBehaviour
                 break;
             case Weather.WeatherType.Snow:
                 snow.SetActive(true);
-                if (snowParticle != null) snowParticle.Play();
+                snowParticle.Play();
                 break;
         }
 
     }
     void LateUpdate()
     {
+        // 파티클 효과 위치를 카메라 기준으로 조정
+        rainParticle.transform.position = cameraTransform.position + new Vector3(0, 10, 0); 
 
-        if (rainParticle != null)
-            rainParticle.transform.position = cameraTransform.position + new Vector3(0, 10, 0); 
-
-        if (snowParticle != null)
-            snowParticle.transform.position = cameraTransform.position + new Vector3(0, 10, 0); 
+        snowParticle.transform.position = cameraTransform.position + new Vector3(0, 10, 0); 
     }
 }
